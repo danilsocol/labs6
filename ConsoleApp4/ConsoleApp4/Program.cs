809d2a8996cd659c14db72783bea3ccd646f8ab6
danilsocol <danilsocol-2002@mail.ru>
2020-10-25 17:51:25 +0500

    2

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -1,45 +1,234 @@
 ﻿using System;
+using System.ComponentModel.Design;
 
 namespace ConsoleApp4
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {
-            int[,] mainMas = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } }; // первый массив
-            int[,] noMainMas = { { 1, 2, 3 }, { 4, 5, 6 } };                            // второй массив
+            // // Слабонервным не смотреть!!!!
 
+            Console.WriteLine("Выберите пример от 1 до 7");
+            int Example = Convert.ToInt32(Console.ReadLine());
+            Selection(Example);
+        }
+
+        static void Selection(int Example)
+        {
+            switch (Example)
+            {
+                case 1:
+                    {
+                        
+                        int[] mainMas = { 1, 2, 3, 4, 5 };
+                        int[] noMainMas = { 2, 3 };
+                        Outout(mainMas, noMainMas);
+                    }
+                    break;
+
+                case 2:
+                    {
+                        int[] mainMas = { 1, 2, 3, 4, 5 };
+                        int[] noMainMas = { 1, 2, 4 };
+                        Outout(mainMas, noMainMas);
+                    }
+                    break;
+
+                case 3:
+                    {
+                        int[] mainMas = { 1, 2, 3, 4, 5 };
+                        int[] noMainMas = { 2, 6 };
+                        Outout(mainMas, noMainMas);
+                    }
+                    break;
+
+                case 4:
+                    {
+                        int[,] mainMas = { { 1, 2, 3 }, { 3, 4, 5 }, { 7, 8, 9 } };
+                        int[,] noMainMas = { { 3, 4, 5 }, { 7, 8, 9 } };
+                        Output(mainMas, noMainMas);
+                    }
+                    break;
+
+                case 5:
+                    {
+                        int[,] mainMas = { { 1, 2, 3 }, { 3, 4, 5 }, { 7, 8, 9 } };
+                        int[,] noMainMas = { { 3, 4, 5 }};
+                        Output(mainMas, noMainMas);
+                    }
+                    break;
+
+                case 6:
+                    {
+                        int[,] mainMas = { { 1, 2, 3 }, { 3, 4, 5 }, { 7, 8, 9 } };
+                        int[] noMainMas = {  3, 4, 5  };
+                        Outout(mainMas, noMainMas);
+                    }
+                    break;
+
+                case 7:
+                    {
+                        int[,] mainMas = { { 1, 2, 3 }, { 3, 4, 5 }, { 7, 8, 9 } };
+                        int[,] noMainMas = { { 3, 4, 5 }, { 6, 7, 8 } };
+                        Output(mainMas, noMainMas);
+                    }
+                    break;
+            }
+        }
+        static bool Resulation(int Matching, int leghtNoMainMas)
+        {
+            return Matching == leghtNoMainMas;
+        }
+
+        static void Outout(int[] mainMas,int[] noMainMas)
+        {
             int rowsMainMas = mainMas.GetUpperBound(0) + 1;
             int rowsNoMainMas = noMainMas.GetUpperBound(0) + 1;
 
-            int rightNum = Check(rowsMainMas, rowsNoMainMas, mainMas, noMainMas);
-            Console.WriteLine(Resulation(rightNum, noMainMas));
+            int beforeNoMainMas = Check(rowsMainMas, rowsNoMainMas, mainMas, noMainMas);
+            int Matching = NeedNumTogether(beforeNoMainMas, rowsNoMainMas, mainMas, noMainMas);
 
-        }
-        static bool Resulation(int rightNum, int[,] noMainMas)
-        {
-            return rightNum == noMainMas.Length;
+            int leghtNoMainMas = noMainMas.Length;
+            Console.WriteLine(Resulation(Matching, leghtNoMainMas));
         }
 
-        static int Check(int rowsMainMas, int rowsNoMainMas, int[,] mainMas, int[,] noMainMas)
+        static void Output(int[,] mainMas, int[,] noMainMas)
         {
-            int rightNum = 0;
+            int rowsMainMas = mainMas.GetUpperBound(0) + 1;
+            int rowsNoMainMas = noMainMas.GetUpperBound(0) + 1;
+
+            int beforeNoMainMas = Check(rowsMainMas, rowsNoMainMas, mainMas, noMainMas);
+            int Matching = NeedNumTogether(beforeNoMainMas, rowsNoMainMas, mainMas, noMainMas);
+
+            int leghtNoMainMas = noMainMas.Length;
+            Console.WriteLine(Resulation(Matching, leghtNoMainMas));
+        }
+
+        static void Outout(int[,] mainMas, int[] noMainMas)
+        {
+            int rowsMainMas = mainMas.GetUpperBound(0) + 1;
+            int rowsNoMainMas = noMainMas.GetUpperBound(0) + 1;
+
+            int beforeNoMainMas = Check(rowsMainMas, rowsNoMainMas, mainMas, noMainMas);
+            int Matching = NeedNumTogether(beforeNoMainMas, rowsNoMainMas, mainMas, noMainMas);
+
+            int leghtNoMainMas = noMainMas.Length;
+            Console.WriteLine(Resulation(Matching, leghtNoMainMas));
+        }
+
+        static int Check(int rowsMainMas, int rowsNoMainMas, int[] mainMas, int[] noMainMas)
+        {
+            int beforeNum = 0;
 
             for (int i = 0; i < rowsMainMas; i++)
             {
-                for (int a = 0; a < rowsNoMainMas; a++)
+                if (mainMas[i] == noMainMas[0])
                 {
-                    for (int j = 0; j < noMainMas.Length / rowsNoMainMas; j++)
-                    {
-                        if (mainMas[i, j] == noMainMas[a, j])
-                        {
-                            rightNum++;
-                        }
-                    }
+                    beforeNum = i;
+                    break;
                 }
             }
-            return rightNum;
+            return beforeNum;
         }
 
+        static int NeedNumTogether(int beforeNoMainMas, int rowsNoMainMas, int[] mainMas, int[] noMainMas)
+        {
+            int Matching = 0;
+
+            for (int i = 0; i < rowsNoMainMas; i++)
+            {
+                if (mainMas[i + beforeNoMainMas] == noMainMas[i])
+                {
+                    Matching++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return Matching;
+        }
+
+        static int Check(int rowsMainMas, int rowsNoMainMas, int[,] mainMas, int[,] noMainMas) //561616
+        {
+            int beforeNum = -1;
+
+            for (int i = 0; i < rowsMainMas; i++)
+            {
+                if (mainMas[i, 0] == noMainMas[0, 0])
+                {
+                    beforeNum = i;
+                    break;
+                }
+            }
+            return beforeNum;
+        }
+
+        static int NeedNumTogether(int beforeNoMainMas, int rowsNoMainMas, int[,] mainMas, int[,] noMainMas)
+        {
+            int Matching = 0;
+
+            for (int i = 0; i < rowsNoMainMas; i++)
+            {
+                bool isBreak = false;
+                for (int j = 0; j < noMainMas.Length / rowsNoMainMas; j++)
+                {
+                    if (mainMas[i + beforeNoMainMas, j] == noMainMas[i, j])
+                    {
+                        Matching++;
+                    }
+                    else
+                    {
+                        isBreak = true;
+                        break;
+                    }
+                }
+                if (isBreak)
+                    break;
+            }
+            return Matching;
+        }
+
+        static int Check(int rowsMainMas, int rowsNoMainMas, int[,] mainMas, int[] noMainMas)
+        {
+            int beforeNum = -1;
+
+            for (int i = 0; i < rowsMainMas; i++)
+            {
+                bool isBreak = false;
+                for (int j = 0; j < noMainMas.Length / rowsNoMainMas; j++)
+                {
+                    if (mainMas[i, j] == noMainMas[j])
+                    {
+                        beforeNum = i;
+                        isBreak = true;
+                        break;
+                    }
+                }
+                if (isBreak) { break; }
+
+            }
+            return beforeNum;
+        }
+
+        static int NeedNumTogether(int beforeNoMainMas, int rowsNoMainMas, int[,] mainMas, int[] noMainMas)
+        {
+            int Matching = 0;
+
+            for (int i = beforeNoMainMas; i < rowsNoMainMas; i++)
+            {
+                for (int j = 0; j < noMainMas.Length; j++)
+                {
+                    if (mainMas[i, j] == noMainMas[j])
+                    {
+                        Matching++;
+                    }
+                }
+
+            }
+            return Matching;
+        }
     }
 }
+
